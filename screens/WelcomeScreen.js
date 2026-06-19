0f690069743ae0bebdc9b7f6f4b0e370fbb3913a
Chloe Pham <chloe.ph2906@gmail.com>
2026-06-19 13:41:59 +1000

    Pass token into http to get protected resource (Which set the rule in DB)

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "../store/auth-context";
+
+const DATABASE_URL = process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL;
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+  const authContext = useContext(AuthContext);
+  const authToken = authContext.token;
+  useEffect(() => {
+    axios
+      .get(`${DATABASE_URL}/message.json?auth=${authToken}`)
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [authToken]);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
@@ -14,13 +30,13 @@ export default WelcomeScreen;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 32,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
 });
